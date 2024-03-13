98fe6fbab25f8a3d012accc85176462e2d6ddf84
Marek Mościchowski <marek.moscichowski@miquido.com>
2024-03-13 08:36:14 +0100

    fix: validate

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,8 +1,8 @@
 module "ecs_error_detection" {
   source = "../../"
 
-  ecs_cluster_id = "example-cluster"
-  environment    = "exammple"
-  project        = "example"
-  sns_topic_arn  = "arn:aws:sns:eu-central-1:123456789012:example"
+  ecs_cluster_name = "example-cluster"
+  environment      = "exammple"
+  project          = "example"
+  sns_topic_arn    = "arn:aws:sns:eu-central-1:123456789012:example"
 }
